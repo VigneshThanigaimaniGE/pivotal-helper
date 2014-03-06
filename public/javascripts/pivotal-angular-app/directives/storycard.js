a0f1d1a7d976fca5be58238611d94f1e65c3e677
VigneshPT <vignesh294@gmail.com>
2014-03-06 18:58:22 +0530

    Re-adjusting threshold height for single page printouts

--- public/javascripts/pivotal-angular-app/directives/storycard.js
+++ public/javascripts/pivotal-angular-app/directives/storycard.js
@@ -1,4 +1,4 @@
-app.directive('storycard',[function(){
+app.directive('storycard',['$timeout',function($timeout){
 	return {
 		priority:10000,
 		restrict: 'E',
@@ -6,28 +6,31 @@ app.directive('storycard',[function(){
 		replace:true,
 		controller:function($scope){
 			$scope.$on("onrepeatend",function(){
-				$storiesDom = $('.printMe');
-				$storiesDom.each(function(index,element){
-					$boardView = $(element).find('.board-view');
-					$deskView =$(element).find('.desk-view');
-					$secondDivider = $(element).find('.invisible-divider');
-					//check if the cumulative height of board and desk view is greater than 800px
-					//assuming that 800px is threshold value for a page.
-					if($boardView.height() + $deskView.height() >= 800){
-						//increase the divider height in such a way that the boardview consumes the whole page
-						$(element).find(".divider").height(850 - $boardView.height());
-						//increase the deskview height in such a way, that it consumes the whole page. 
-						adjustSecondCardOnScreen($deskView);
-						adjustSecondCardOnScreen($deskView);
-						adjustSecondCardOnScreen($deskView);
-						// if($deskView.height()<=600) 
-						// 	$secondDivider.height(700 - $deskView.height());
-						// else if($deskView.height() >=610){
-							
-						// 	$deskView.find('.task').css("font-size","12px")
-						// }
-					}
+				$timeout(function(){
+					$storiesDom = $('.printMe');
+					$storiesDom.each(function(index,element){
+						$boardView = $(element).find('.board-view');
+						$deskView =$(element).find('.desk-view');
+						$secondDivider = $(element).find('.invisible-divider');
+						//check if the cumulative height of board and desk view is greater than 800px
+						//assuming that 800px is threshold value for a page.
+						if($boardView.height() + $deskView.height() >= 800){
+							//increase the divider height in such a way that the boardview consumes the whole page
+							$(element).find(".divider").height(950 - $boardView.height());
+							//increase the deskview height in such a way, that it consumes the whole page. 
+							adjustSecondCardOnScreen($deskView);
+							adjustSecondCardOnScreen($deskView);
+							adjustSecondCardOnScreen($deskView);
+							// if($deskView.height()<=600) 
+							// 	$secondDivider.height(700 - $deskView.height());
+							// else if($deskView.height() >=610){
+								
+							// 	$deskView.find('.task').css("font-size","12px")
+							// }
+						}
+					});
 				});
+				
 			});
 		}
 	}
